19aed68584375d59503b51fc10acb1c60a4cbf15
Carl Thuringer <carl@groupon.com>
2016-03-19 08:58:40 -0500

    Add Router

--- app/Main.elm
+++ app/Main.elm
@@ -10,24 +10,65 @@ import StartApp.Simple exposing (start)
 import CountDown exposing (init, view, Action(ClockTick))
 import Navigation exposing (view)
 
-type alias Model = { countDown : CountDown.Model }
+-- ROUTING IMPORTS
+import Hop
+import Hop.Matchers exposing (match1, match2)
+import Hop.Navigate exposing (navigateTo)
+import Hop.Types exposing (Router, PathMatcher, Location, newLocation)
+import Task exposing (Task)
+
+
+type alias Model =
+  { countDown: CountDown.Model
+  , location: Location
+  , route: Route }
 
 type Action
   = CountDown CountDown.Action
   | TimeUpdate Int
+  | ApplyRoute (Route, Location)
+
+-- ROUTING
+type Route
+  = HomeRoute
+  | NotFoundRoute
+
+matcherHome : PathMatcher Route
+matcherHome = match1 HomeRoute "/"
+
+matchers : List (PathMatcher Route)
+matchers =
+  [ matcherHome ]
+
+router : Router Route
+router = Hop.new
+  { matchers = matchers
+  , notFound = NotFoundRoute }
+
+boxedRouterSignal : Signal Action
+boxedRouterSignal = Signal.map ApplyRoute router.signal
 
 init : Model
-init = { countDown = CountDown.init }
+init =
+  { countDown = CountDown.init
+  , location = newLocation
+  , route = HomeRoute }
 
 view : Model -> Html
-view state =
+view model =
+  case model.route of
+    HomeRoute -> homeView model
+    NotFoundRoute -> p [] [ text "Where are we" ]
+
+homeView : Model -> Html
+homeView model =
   div [] [
     Navigation.view,
     div [ class "container" ] [
       div [] [
-        h1 [] [ text "Carl Thuringer & Nicole Cammarata"],
-        p [ class "lead"] [
-          CountDown.view state.countDown,
+        h1 [] [ text "Carl & Nicole"],
+        p [ class "lead" ] [
+          CountDown.view model.countDown,
           text " Until they get married!"
         ]
       ]
@@ -39,6 +80,7 @@ update action model =
   case action of
     CountDown act -> { model | countDown = CountDown.update act model.countDown }
     TimeUpdate timeInt -> { model | countDown = CountDown.update (ClockTick (toFloat timeInt)) model.countDown }
+    ApplyRoute (route, location) -> { model | route = route, location = location }
 
 changes : Signal Action
 changes = Signal.mergeMany
@@ -49,5 +91,8 @@ state = Signal.foldp update init changes
 
 type TimeSeconds = Int
 port hostClock : Signal Int
+
+port routeRunTask : Task () ()
+port routeRunTask = router.run
 
 main = Signal.map view state

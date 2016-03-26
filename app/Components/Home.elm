765829952361cec6792938bec24b8dbc661127de
Carl Thuringer <carl@groupon.com>
2016-03-26 08:38:15 -0500

    Add party page content

--- app/Components/Home.elm
+++ app/Components/Home.elm
@@ -3,10 +3,17 @@ module Components.Home where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import CountDown exposing (view)
+import Components.HomeCSS
 
 view model =
   div [] [
     h1 [] [ text "Carl & Nicole"],
+    div [ class "wedding-colors" ] [
+      div [ class "rose-gold" ] [],
+      div [ class "peach" ] [],
+      div [ class "serenity-blue" ] [],
+      div [ class "medium-grey" ] []
+    ],
     p [ class "lead" ] [
       CountDown.view model.countDown,
       text " Until they get married!"
--- app/Components/Party.elm
+++ app/Components/Party.elm
@@ -1,5 +1,73 @@
 module Components.Party where
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Markdown exposing (toHtml)
 
-view model = text "Party"
+view model = div [ id "party" ]
+  [ h2 [] [ text "Wedding Party" ]
+  , h3 [] [ text "Bride's" ]
+  , partyMember "Jill Cammarata" "placeholder" jill
+  , partyMember "Paul Cammarata" "placeholder" paul
+  , partyMember "Joanna Nichols" "placeholder" joanna
+  , partyMember "Kate Slingerland" "placeholder" kate
+  , partyMember "Carolyn Radville" "placeholder" carolyn
+  , h3 [] [ text "Groom's" ]
+  , partyMember "Brian Thuringer" "placeholder" brian
+  , partyMember "Scott McAnally" "placeholder" scott
+  , partyMember "Dustin Mitchell" "placeholder" dustin
+  , partyMember "David Abdemoulie" "placeholder" david
+  , partyMember "Sarah Killian" "placeholder" sarah
+  ]
+
+partyMember name image content =
+  article []
+    [ h4 [] [ text name ]
+    , p []
+      [ img [ src "http://www.fillmurray.com/g/200/240" ] []
+      , content
+      ]
+    ]
+
+jill = toHtml """
+Some nice words about Jill go here. How's that for you?
+## What if we write a list?
+  * Listing
+  * Listing more
+"""
+
+paul = toHtml """
+Words about Paul
+"""
+
+joanna = toHtml """
+joanna
+"""
+
+kate = toHtml """
+kate
+"""
+
+carolyn = toHtml """
+carolyn
+"""
+
+brian = toHtml """
+brian
+"""
+
+scott = toHtml """
+scott
+"""
+
+dustin = toHtml """
+dustin
+"""
+
+david = toHtml """
+david
+"""
+
+sarah = toHtml """
+sarah
+"""

fec3b09d6619cfae6e54cf81b13c2c81a42888d4
necamma <cammarata.nicole@gmail.com>
2016-05-30 18:09:23 -0500

    Removed "Munster" in venue title

--- src/Components/Venue.elm
+++ src/Components/Venue.elm
@@ -11,7 +11,7 @@ view : Html Msg
 view =
   div
     []
-    [ h1 [] [ text "Munster Center for Visual and Performing Arts" ]
+    [ h1 [] [ text "Center for Visual and Performing Arts" ]
     , iframe
         [ src "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.4279945435705!2d-87.50520228392293!3d41.55998077924856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8811e0638984d1c5%3A0xc75da4bbc1bab9a1!2sCenter+For+Visual+%26+Performing+Arts!5e0!3m2!1sen!2sus!4v1463348007804"
         , width 600

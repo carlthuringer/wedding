bc83bc0dea19e970ec510aafffd32c70b37cf7e3
Carl Thuringer <carlthuringer@gmail.com>
2016-07-24 17:27:46 -0500

    Now we just have to resize the files

--- script/photos.js
+++ script/photos.js
@@ -1,29 +1,43 @@
 (function() {
-    'use strict';
-    const root = require('./helpers.js').root;
-    const path = require('path');
-    const _ = require('lodash');
-    const fs = require('fs');
-    // read teh config file to get the list of photo paths and their properties
-    // Convert the photos and write them to the target directory
-    // Load the template
-    // Render out to the destination
+  'use strict';
+  const root = require('./helpers.js').root;
+  const path = require('path');
+  const _ = require('lodash');
+  const fs = require('fs-extra');
+  // read teh config file to get the list of photo paths and their properties
+  // Convert the photos and write them to the target directory
+  // Load the template
+  // Render out to the destination
 
-    let options = JSON.parse(fs.readFileSync(root('script', 'photos.json')).toString());
-    // first let's just copy the photo
-    let photos = options.photos.map(function(photo) {
-        let base = path.basename(photo.file);
-        // Just copy file
-        fs.createReadStream(root('.', photo.file)).pipe(fs.createWriteStream(root(options.destination, base)));
-        return {
-            src: path.join("assets", base),
-            srcset: [path.join("assets", base) + " 100w"].join(',')
-        };
+  let options = JSON.parse(fs.readFileSync(root('script', 'photos.json')).toString());
+  // Make sure the destination directory exists
+
+  fs.mkdirsSync(options.destination);
+
+  let photos = options.photos.map(function(photo) {
+    let base = path.basename(photo.file);
+    // This just copies the file.
+    let infile = fs.createReadStream(root('.', photo.file));
+    // Now we'll make a copy for each size. but we don't actually resize yet.
+    options.sizes.forEach(function(size) {
+      infile.pipe(writeFile(base + "." + size));
     });
 
-    let template = fs.readFileSync(root('script', 'photos.elm.ejs'));
-    let compiled = _.template(template);
-    let rendered = compiled({ photos: photos });
-    fs.writeFileSync(root('src', 'Photos.elm'), rendered);
+    // Though first perhaps we can just write files.
+
+    return {
+      src: path.join("assets", base),
+      srcset: [path.join("assets", base) + " 100w"].join(',')
+    };
+  });
+
+  let template = fs.readFileSync(root('script', 'photos.elm.ejs'));
+  let compiled = _.template(template);
+  let rendered = compiled({ photos: photos });
+  fs.writeFileSync(root('src', 'Photos.elm'), rendered);
+
+  function writeFile(fileName) {
+    return fs.createWriteStream(root(options.destination, fileName));
+  }
 
 })();

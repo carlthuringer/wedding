a997b87d0f4ef1aaa89c86ffc8c75fe873d9a040
Carl Thuringer <carlthuringer@gmail.com>
2016-07-25 08:45:17 -0500

    Sometimes it doesn't create the image... and its slow

--- src/Photos.elm
+++ src/Photos.elm
@@ -10,9 +10,8 @@ getSrc (path, _) =
 
 photos : List (SrcPath, SrcSet)
 photos =
-    [("/assets/320/CN-3.jpg", "/assets/320/CN-3.jpg 320w,/assets/700/CN-3.jpg 700w,/assets/1250/CN-3.jpg 1250w")
+    [("/assets/320/CN.jpg", "/assets/320/CN.jpg 320w,/assets/700/CN.jpg 700w,/assets/1250/CN.jpg 1250w")
     ,("/assets/320/CN-11.jpg", "/assets/320/CN-11.jpg 320w,/assets/700/CN-11.jpg 700w,/assets/1250/CN-11.jpg 1250w")
-    ,("/assets/320/CN.jpg", "/assets/320/CN.jpg 320w,/assets/700/CN.jpg 700w,/assets/1250/CN.jpg 1250w")
     ,("/assets/320/CN-4.jpg", "/assets/320/CN-4.jpg 320w,/assets/700/CN-4.jpg 700w,/assets/1250/CN-4.jpg 1250w")
     ,("/assets/320/CN-33.jpg", "/assets/320/CN-33.jpg 320w,/assets/700/CN-33.jpg 700w,/assets/1250/CN-33.jpg 1250w")
     ,("/assets/320/CN-7.jpg", "/assets/320/CN-7.jpg 320w,/assets/700/CN-7.jpg 700w,/assets/1250/CN-7.jpg 1250w")

a0a89663b4f6daf463c81ef6f49c7514e894493a
Carl Thuringer <carlthuringer@gmail.com>
2017-02-05 09:42:37 -0600

    Change source of rsvpify scripts to https

--- src/Components/RSVP.elm
+++ src/Components/RSVP.elm
@@ -27,7 +27,7 @@ rsvpifyEmbedGarbage =
             , attribute "allowtransparency" "true"
             , attribute "frameborder" "0"
             , attribute "scrolling" "no"
-            , src "http://carlandnicole.app.rsvpify.com/?embed=1&amp;js=1"
+            , src "https://carlandnicole.app.rsvpify.com/?embed=1&amp;js=1"
             ]
             []
           -- <iframe id="RSVPifyIFrame" style="width: 100%; border: none; overflow: hidden;" onload="window.parent.scrollTo(0,0)" allowtransparency="true" src="http://carlandnicole.app.rsvpify.com/?embed=1&amp;js=1" frameborder="0" scrolling="no"></iframe>

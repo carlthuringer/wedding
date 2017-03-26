69f9277314d56b6adb2cfd6e38a54efdd6f1ec58
Carl Thuringer <carlthuringer@gmail.com>
2017-03-26 11:40:53 -0500

    Add urgency links to the homepage

--- src/Components/Home.elm
+++ src/Components/Home.elm
@@ -6,6 +6,9 @@ import CountDown exposing (view)
 import Update exposing (Model)
 import Messages exposing (Msg)
 import Photos exposing (toImg, engagement)
+import Markdown exposing (toHtml)
+import Common.View exposing (link)
+import Routes exposing (Sitemap(..))
 
 
 view : Model -> Html Msg
@@ -17,5 +20,12 @@ view model =
         , p [ class "center-auto" ]
             [ CountDown.view model.countDown
             , text " Until they get married!"
+            ]
+        , p [ class "center-auto" ]
+            [ text "Please "
+            , link model.route RSVPR "RSVP"
+            , text " by May 5th. The last day to "
+            , link model.route TravelAndAccomodationR "book your hotel room"
+            , text " is Friday, May 5th."
             ]
         ]
